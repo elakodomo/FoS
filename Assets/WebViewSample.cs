4771fa312403f06d8d8a37bc6db93d038e0cc09a
CRAZY-RICEBALL <hinatablackmita@gmail.com>
2024-06-27 20:38:31 +0900

    ssss

---
+++ Assets/WebViewSample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_2018_4_OR_NEWER
+using UnityEngine.Networking;
+#endif
+using UnityEngine.UI;
+
+public class WebViewSample : MonoBehaviour
+{
+    WebViewObject webViewObject;
+
+    void Start()
+    {
+        webViewObject = (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+        webViewObject.Init(
+            ld: (msg) => Debug.Log(string.Format("CallOnLoaded[{0}]", msg)),
+            enableWKWebView: true);
+
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        webViewObject.bitmapRefreshCycle = 1;
+#endif
+        webViewObject.LoadURL("https://www.google.co.jp");
+
+        webViewObject.SetVisibility(true);
+    }
+}
